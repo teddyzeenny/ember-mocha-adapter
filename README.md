5a6487a26304218b239822f4ef2c2fe6af3f0d8b
Teddy Zeenny <teddyzeenny@gmail.com>
2013-05-23 22:07:50 +0300

    Remove asynchronous management from mocha testing

--- README.md
+++ README.md
@@ -2,3 +2,48 @@ Ember Mocha Adapter
 -------------------
 
 A mocha adapter for ember-testing.
+
+This adapter makes async testing ember apps with mocha
+easier.
+
+It gets rid of the `done()` callback and lets
+you test without worrying whether your tests
+are sync or async.
+
+Example:
+
+```javascript
+
+describe("Adding a post", function() {
+
+  beforeEach(function() {
+    visit('posts/new');
+  });
+
+
+  it("should take me to a form", function() {
+    find('form').should.exist;
+  });
+
+  it("should not submit with an empty name", function() {
+    click('.submit').then(function() {
+      App.get('currentPath').should.equal('posts.new')
+    });
+  });
+
+  it("should create a post on submit", function() {
+    fillIn('.name', 'Test Post').then(function() {
+      return click('.submit');
+    }).then(function() {
+      find('.post').should.exist
+      find('.post-name').text().should.equal('Test Post');
+    });
+  });
+
+  afterEach(function() {
+    App.reset();
+  });
+
+});
+
+```
--- adapter.js
+++ adapter.js
@@ -1,3 +1,90 @@
 (function() {
-  Ember.Test.MochaAdapter = Ember.Test.Adapter.extend();
+  var done, doneTimeout, countAsync;
+
+  done = null;
+  doneTimeout = null;
+  countAsync = 0;
+
+  Ember.Test.MochaAdapter = Ember.Test.Adapter.extend({
+    asyncStart: function() {
+      countAsync++;
+      clearTimeout(doneTimeout);
+    },
+    asyncEnd: function() {
+      countAsync--;
+      if (done && countAsync === 0) {
+        doneTimeout = setTimeout(function() {
+          done();
+          done = null;
+        });
+      }
+    },
+    exception: function(reason) {
+      var error;
+
+      error = new Error(reason);
+      if (done) {
+        done(error);
+      } else {
+        setTimeout(function() {
+          throw error;
+        });
+      }
+    }
+  });
+
+
+  // Overwrite the default Mocha
+  // methods to get rid of manually taking
+  // care of the `done()` callback
+
+  var oldBefore, oldBeforeEach,
+      oldAfter, oldAfterEach, oldIt;
+
+  oldBefore = window.before;
+  window.before = fixAsync(oldBefore);
+
+  oldBeforeEach = window.beforeEach;
+  window.beforeEach = fixAsync(oldBeforeEach);
+
+  oldAfter = window.after;
+  window.after = fixAsync(oldAfter);
+
+  oldAfterEach = window.afterEach;
+  window.afterEach = fixAsync(oldAfterEach);
+
+
+
+  function fixAsync(method) {
+    return function(fn) {
+      if (fn.length === 1) {
+      return method(fn);
+      }
+      return method(function(d) {
+        done = d;
+        fn();
+        if (countAsync === 0) {
+          d();
+          done = null;
+        }
+      });
+    };
+  }
+
+  oldIt = window.it;
+
+  window.it = function(desc, fn) {
+    if (fn.length === 1) {
+      return oldIt(desc, fn);
+    }
+    return oldIt(desc, function(d) {
+      done = d;
+      fn();
+      if (countAsync === 0) {
+        d();
+        done = null;
+      }
+    });
+  };
+
 }());
